a3117ac48c80ce12585d7a1cd014509fdf680856
NagaTarun <76032566+rnagatarun@users.noreply.github.com>
2025-08-12 15:05:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,14 @@ st.write('Name on your smoothie will be: ', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+
+#Convert the Snowparl DataFrame to a pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredient_list = st.multiselect('Choose up to 5 ingredients: '
